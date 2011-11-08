351c399564d07d4fcd3a37440a5157fb2bc045b6
cbolk <cristiana.bolchini@polimi.it>
2011-11-08 13:05:23 +0100

    repository restructuring -- no code change
    rebuilding the original domjudge application structure

---
+++ judgehost/lib/judge/compile_haskell.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# Haskell compile wrapper-script for 'compile.sh'.
+# See that script for syntax and more info.
+
+SOURCE="$1"
+DEST="$2"
+
+# -Wall:	Report all warnings
+# -O:		Optimize
+# -static:	Static link Haskell libraries
+# -optl-static:	Pass '-static' option to the linker
+# -optl-pthread: Pass '-pthread' option to the linker (see Debian bug #593402)
+ghc -Wall -Wwarn -O -static -optl-static -optl-pthread -o $DEST $SOURCE
+exitcode=$?
+
+# clean created files:
+rm -f $DEST.o Main.hi
+
+exit $exitcode
